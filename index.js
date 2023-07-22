1985dc90b07c63c28275d29027ae90a6a901af9f
unknown <prathamkakkad18@gmail.com>
2023-07-22 17:19:13 +0530

    javascript updated

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
-var audio = new Audio("sounds/crash.mp3");
-
 for (var i = 0; i < document.querySelectorAll(".set button").length; i++) {
   document
     .querySelectorAll(".set button")
     [i].addEventListener("click", function () {
-      this.style.color = "white";
-      // audio.play();
+      var buttonInnerHtml = this.innerHTML;
+
+      makeSound(buttonInnerHtml);
     });
 }
 
-function HouseKeeper(name, age, location) {
-  this.name = name;
-  this.age = age;
-  this.location = location;
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      console.log("Hewl");
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+
+    default:
+      this.innerHTML = "gayaaaa";
+      break;
+  }
 }
